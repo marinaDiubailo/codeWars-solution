ab2493e2a217c2be1dbd2efcb3c12284b767a6cc
Marina Diubailo <marinadiubailo@gmail.com>
2023-12-22 15:21:11 +0300

    add new kata's solutions

---
+++ TotalAmountOfPoints.js
@@ -0,0 +1,59 @@
+/**
+ * DESCRIPTION:
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings.
+ Each match is represented by a string in the format "x:y",
+  where x is our team's score and y is our opponents score.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that
+ takes this collection and returns the number of
+  points our team (x) got in the championship by the rules
+   given above.
+
+Notes:
+
+our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+ */
+
+function points(games) {
+    let count = 0;
+
+    for (let i = 0; i < games.length; i++) {
+        let points = games[i].split(':');
+
+        if (points[0] > points[1]) {
+            count += 3;
+        }
+
+        if (points[0] === points[1]) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+console.log(
+    points([
+        '1:0',
+        '2:0',
+        '3:0',
+        '4:0',
+        '2:1',
+        '3:1',
+        '4:1',
+        '3:2',
+        '4:2',
+        '4:3',
+    ]),
+);
