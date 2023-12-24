389ad1a14dd8b6ec9b329f90ef0a99dfa81a3c6e
Marina Diubailo <marinadiubailo@gmail.com>
2023-12-24 19:27:33 +0300

    add kata

---
+++ BilliardsTriangle.js
@@ -0,0 +1,34 @@
+/**
+ * DESCRIPTION:
+Remember the triangle of balls in billiards?
+ To build a classic triangle (5 levels) you need 15 balls.
+  With 3 balls you can build a 2-level triangle, etc.
+
+For more examples,
+
+pyramid(1) == 1
+
+pyramid(3) == 2
+
+pyramid(6) == 3
+
+pyramid(10) == 4
+
+pyramid(15) == 5
+Write a function that takes number of balls (≥ 1)
+ and calculates how many levels you can build a triangle.
+ */
+
+function pyramid(balls) {
+    let count = 0;
+
+    let ballsInLevel = 1;
+
+    while (ballsInLevel <= balls) {
+        count++;
+        balls -= ballsInLevel;
+        ballsInLevel++;
+    }
+
+    return count;
+}
