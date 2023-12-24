9103d537d132847aa8eab8e30da1b0132871efba
Marina Diubailo <marinadiubailo@gmail.com>
2023-12-24 16:29:08 +0300

    add new kata

---
+++ MergingSortedIntegerArrays.js
@@ -0,0 +1,11 @@
+/**
+ * DESCRIPTION:
+Write a function that merges two sorted arrays into a single one. The arrays only contain integers.
+ Also, the final outcome must be sorted and not have any duplicate.
+ */
+
+function mergeArrays(a, b) {
+    const sorted = a.concat(b).sort((a, b) => a - b);
+
+    return [...new Set(sorted)];
+}
